4db9cfdc1f7859322f87e25d1a010212f1878e3e
Dominic <git@msrd0.de>
2020-11-12 03:19:36 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate log;
+
+use sqlx::PgPool;
+use std::env;
+
+mod routing;
+
+mod embedded {
+	refinery::embed_migrations!("./migrations");
+}
+
+lazy_static! {
+	static ref POOL: PgPool =
+		PgPool::connect_lazy(&env::var("DATABASE_URL").expect("DATABASE_URL must point to a postgres database"))
+			.expect("Failed to connect to database");
+}
+
+fn main() {
+	pretty_env_logger::init_timed();
+	if let Err(err) = dotenv::dotenv() {
+		warn!("Unable to read `.env' file: {}", err);
+	}
+
+	let mut db_conf =
+		refinery::config::Config::from_env_var("DATABASE_URL").expect("DATABASE_URL must point to a postgres database");
+	embedded::migrations::runner()
+		.run(&mut db_conf)
+		.expect("Failed to run migrations");
+
+	let router = routing::router();
+	let addr = "[::]:7181";
+	gotham::start(addr, router);
+}
---
+++ src/routing.rs
@@ -0,0 +1,19 @@
+use gotham::{
+	handler::HandlerError,
+	helpers::http::response::create_empty_response,
+	hyper::{header::LOCATION, Body, Response, StatusCode},
+	middleware::{
+		logger::RequestLogger,
+		session::{NewSessionMiddleware, SessionData}
+	},
+	pipeline::{new_pipeline, single::single_pipeline},
+	router::{builder::*, Router},
+	state::{FromState, State}
+};
+
+pub(crate) fn router() -> Router {
+	let logger_middleware = RequestLogger::new(Level::Info);
+
+	let (chain, pipelines) = single_pipeline(new_pipeline().add(logger_middleware).build());
+	build_router(chain, pipelines, |route| {})
+}
