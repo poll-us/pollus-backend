d16bb261a1c997fe7b9f46093275d01c6e63294c
Dominic <git@msrd0.de>
2020-11-16 15:22:47 +0100

    adjust the api

--- src/routing/poll.rs
+++ src/routing/poll.rs
@@ -3,39 +3,35 @@ use chrono::{Duration, NaiveDate, NaiveTime};
 use itertools::Itertools;
 use serde::Serialize;
 use sqlx::query;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Resource)]
 #[resource(read)]
 pub(super) struct PollResource;
 
 #[derive(Serialize)]
-struct PollConfig {
-	id: i64,
-	date: NaiveDate,
-	times: Vec<NaiveTime>
-}
-
-#[derive(Serialize)]
-struct PollData {
-	id: i64,
-	firstname: String,
-	lastname: Option<String>,
-	submissions: Vec<PollSubmission>
-}
-
-#[derive(Serialize)]
-struct PollSubmission {
-	id: i64,
-	time: NaiveTime,
+struct PollEntry {
+	user: i64,
 	value: i16
+}
+
+#[derive(Serialize)]
+struct PollConfig {
+	date: NaiveDate,
+	times: HashMap<NaiveTime, Vec<PollEntry>>
+}
+
+#[derive(Serialize)]
+struct PollUser {
+	firstname: String,
+	lastname: Option<String>
 }
 
 #[derive(Serialize)]
 struct Poll {
 	id: String,
-	cfg: Vec<PollConfig>,
-	data: Vec<PollData>
+	cfg: HashMap<i64, PollConfig>,
+	user: HashMap<i64, PollUser>
 }
 
 #[read(PollResource)]
@@ -48,15 +44,16 @@ async fn read(id: String) -> Result<Poll, sqlx::Error> {
 		.map(|record| ((record.id, record.date), record))
 		.into_group_map()
 		.into_iter()
-		.map(|((id, date), records)| PollConfig {
-			id,
-			date,
-			times: records.into_iter().map(|record| record.time).collect()
+		.map(|((id, date), records)| {
+			(id, PollConfig {
+				date,
+				times: records.into_iter().map(|record| (record.time, Vec::new())).collect()
+			})
 		})
-		.collect::<Vec<_>>();
+		.collect::<HashMap<_, _>>();
 
 	// check that the next 7 days are present
-	let dates = cfg.iter().map(|cfg| cfg.date).collect::<HashSet<_>>();
+	let dates = cfg.iter().map(|(_, cfg)| cfg.date).collect::<HashSet<_>>();
 	let now = query!("SELECT now()::DATE;").fetch_one(&*POOL).await?.now.unwrap();
 	for i in 0..7 {
 		let day = now + Duration::days(i);
@@ -66,45 +63,54 @@ async fn read(id: String) -> Result<Poll, sqlx::Error> {
 				.await?
 				.id;
 			query!("INSERT INTO poll_config_time (cfg, time) VALUES ($1, '20:00'), ($1, '21:00'), ($1, '22:00'), ($1, '23:00');", id).execute(&*POOL).await?;
-			cfg.push(PollConfig {
-				id,
+			cfg.insert(id, PollConfig {
 				date: day,
-				times: [20, 21, 22, 23].iter().map(|hour| NaiveTime::from_hms(*hour, 0, 0)).collect()
+				times: [20, 21, 22, 23]
+					.iter()
+					.map(|hour| (NaiveTime::from_hms(*hour, 0, 0), Vec::new()))
+					.collect()
 			});
 		}
 	}
 
 	// pull the data for those configs
-	let cfg_ids = cfg.iter().map(|cfg| cfg.id).collect::<Vec<i64>>();
+	let cfg_ids = cfg.iter().map(|(id, _)| *id).collect::<Vec<i64>>();
+	let mut user_ids: HashSet<i64> = HashSet::new();
 	let data = query!(
-		"SELECT u.id, u.firstname, u.lastname, t.submission, t.value, t.time FROM poll_submission_time t INNER JOIN poll_submission s ON t.submission = s.id INNER JOIN poll_user u ON s.user = u.id WHERE t.value IS NOT NULL AND s.cfg = ANY($1);",
+		"SELECT s.user, s.cfg, t.value, t.time FROM poll_submission_time t INNER JOIN poll_submission s ON t.submission = s.id WHERE t.value IS NOT NULL AND s.cfg = ANY($1);",
 		&cfg_ids
 	).fetch_all(&*POOL).await?;
-	let data = data
+	for record in data {
+		user_ids.insert(record.user);
+		// the construction of cfg and above database query guarantee that nothing here is None
+		cfg.get_mut(&record.cfg)
+			.unwrap()
+			.times
+			.get_mut(&record.time)
+			.unwrap()
+			.push(PollEntry {
+				user: record.user,
+				value: record.value.unwrap() // the query guarantees this is never None
+			});
+	}
+
+	// pull the required user information
+	let user_ids: Vec<i64> = user_ids.into_iter().collect(); // sqlx can't deal with sets
+	let user = query!(
+		"SELECT u.id, u.firstname, u.lastname FROM poll_user u WHERE u.id = ANY($1);",
+		&user_ids
+	)
+	.fetch_all(&*POOL)
+	.await?;
+	let user = user
 		.into_iter()
 		.map(|record| {
-			(
-				(record.id, record.firstname, record.lastname),
-				(record.submission, record.value, record.time)
-			)
-		})
-		.into_group_map()
-		.into_iter()
-		.map(|((id, firstname, lastname), submissions)| PollData {
-			id,
-			firstname,
-			lastname,
-			submissions: submissions
-				.into_iter()
-				.map(|(id, value, time)| PollSubmission {
-					id,
-					// the database query guarantees that this is never null
-					value: value.unwrap(),
-					time
-				})
-				.collect()
+			(record.id, PollUser {
+				firstname: record.firstname,
+				lastname: record.lastname
+			})
 		})
 		.collect();
 
-	Ok(Poll { id, cfg, data })
+	Ok(Poll { id, cfg, user })
 }
