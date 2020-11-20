35a144ca6df27b5e439e77306a370af066f54963
Dominic <git@msrd0.de>
2020-11-21 00:19:36 +0100

    add name change endpoint

--- src/routing/profile.rs
+++ src/routing/profile.rs
@@ -3,16 +3,24 @@ use gotham::{
 	middleware::session::SessionData,
 	state::{FromState, State}
 };
-use serde::Serialize;
+use gotham_restful::NoContent;
+use serde::{Deserialize, Serialize};
 use sqlx::query;
 
 #[derive(Resource)]
-#[resource(read_all)]
+#[resource(read_all, change_all)]
 pub(super) struct ProfileResource;
 
 #[derive(Serialize)]
 struct Profile {
 	id: i64,
+	firstname: String,
+	#[serde(skip_serializing_if = "Option::is_none")]
+	lastname: Option<String>
+}
+
+#[derive(Deserialize)]
+struct UpdateProfile {
 	firstname: String,
 	lastname: Option<String>
 }
@@ -31,4 +39,19 @@ async fn read_all(state: &mut State) -> Result<Profile, sqlx::Error> {
 		firstname: user.firstname,
 		lastname: user.lastname
 	})
+}
+
+#[change_all(ProfileResource)]
+async fn change_all(state: &mut State, body: UpdateProfile) -> Result<NoContent, sqlx::Error> {
+	let user_id: &i64 = SessionData::<i64>::borrow_from(state);
+	query!(
+		"UPDATE poll_user SET firstname = $1, lastname = $2 WHERE id = $3",
+		body.firstname,
+		body.lastname,
+		user_id
+	)
+	.execute(&*POOL)
+	.await?;
+
+	Ok(Default::default())
 }
