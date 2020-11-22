a267efb61dc256c76fcebb73c2da5118f705820d
Dominic <git@msrd0.de>
2020-11-22 02:37:21 +0100

    allow changing submissions

--- src/routing/submission.rs
+++ src/routing/submission.rs
@@ -5,7 +5,7 @@ use serde::Deserialize;
 use sqlx::query;
 
 #[derive(Resource)]
-#[resource(create)]
+#[resource(create, change)]
 pub(super) struct SubmissionResource;
 
 #[derive(Deserialize)]
@@ -32,9 +32,12 @@ async fn create(auth: AuthStatus, body: NewSubmission) -> AuthResult<NoContent> 
 	}
 
 	// query stuff
-	let times = query!("SELECT t.time FROM poll_config_time t WHERE t.cfg = $1;", body.poll_cfg)
-		.fetch_all(&*POOL)
-		.await?;
+	let times = query!(
+		"SELECT t.time FROM poll_config_time t WHERE t.cfg = $1 ORDER BY t.time;",
+		body.poll_cfg
+	)
+	.fetch_all(&*POOL)
+	.await?;
 	let times = times.into_iter().map(|record| record.time).collect::<Vec<_>>();
 
 	// insert stuff
@@ -49,6 +52,50 @@ async fn create(auth: AuthStatus, body: NewSubmission) -> AuthResult<NoContent> 
 	// TODO there is a bug with query! that makes this not compile
 	query("INSERT INTO poll_submission_time(submission, cfg, time, value) SELECT $1, $2, UNNEST($3::TIME[]), UNNEST($4::SMALLINT[]);")
 		.bind(id).bind(body.poll_cfg).bind(&times).bind(&body.values).execute(&*POOL).await?;
+
+	Ok(().into())
+}
+
+#[change(SubmissionResource)]
+async fn change(auth: AuthStatus, submission: i64, values: Vec<i16>) -> AuthResult<NoContent> {
+	let user_id = auth.ok()?.sub;
+
+	// only 0, 1 and 2 are allowed
+	if values.iter().any(|v| *v > 2 || *v < 0) {
+		return Err(AuthError::InvalidData);
+	}
+
+	// check that the submission exists
+	let sub = query!("SELECT \"user\" FROM poll_submission WHERE id = $1;", submission)
+		.fetch_optional(&*POOL)
+		.await?;
+	if sub.is_none() {
+		return Err(AuthError::NotFound);
+	} else if user_id != sub.unwrap().user {
+		return Err(AuthError::Forbidden);
+	}
+
+	// query stuff
+	let times = query!(
+		"SELECT t.time FROM poll_submission_time t WHERE t.submission = $1 ORDER BY t.time;",
+		submission
+	)
+	.fetch_all(&*POOL)
+	.await?;
+	let times = times.into_iter().map(|record| record.time).collect::<Vec<_>>();
+
+	// update stuff
+	// TODO there is a bug with query! that makes this not compile
+	query(
+		"UPDATE poll_submission_time AS t SET value = u.value FROM (
+			SELECT UNNEST($1::SMALLINT[]) AS value, UNNEST($2::TIME[]) AS time
+		) AS u WHERE submission = $3 AND t.time = u.time"
+	)
+	.bind(&values)
+	.bind(&times)
+	.bind(submission)
+	.execute(&*POOL)
+	.await?;
 
 	Ok(().into())
 }
